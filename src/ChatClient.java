f81201bc0ed8775cceee21b7bf7e5ff8f0228c13
Mylon <mylonsuren@yahoo.ca>
2018-03-07 16:58:35 -0500

    Minor updates

--- src/ChatClient.java
+++ src/ChatClient.java
@@ -36,9 +36,7 @@ public class ChatClient implements Runnable {
             portNumber = Integer.valueOf(args[1]).intValue();
         }
 
-        /*
-         * Open a socket on a given host and port. Open input and output streams.
-         */
+
         try {
             clientSocket = new Socket(host, portNumber);
             inputLine = new BufferedReader(new InputStreamReader(System.in));
@@ -51,21 +49,16 @@ public class ChatClient implements Runnable {
                     + host);
         }
 
-        /*
-         * If everything has been initialized then we want to write some data to the
-         * socket we have opened a connection to on the port portNumber.
-         */
+
         if (clientSocket != null && os != null && is != null) {
             try {
 
-                /* Create a thread to read from the server. */
                 new Thread(new ChatClient()).start();
                 while (!closed) {
                     os.println(inputLine.readLine().trim());
                 }
-                /*
-                 * Close the output stream, close the input stream, close the socket.
-                 */
+
+
                 os.close();
                 is.close();
                 clientSocket.close();
@@ -75,16 +68,9 @@ public class ChatClient implements Runnable {
         }
     }
 
-    /*
-     * Create a thread to read from the server. (non-Javadoc)
-     *
-     * @see java.lang.Runnable#run()
-     */
+
     public void run() {
-        /*
-         * Keep on reading from the socket till we receive "Bye" from the
-         * server. Once we received that then we want to break.
-         */
+
         String responseLine;
         try {
             while ((responseLine = is.readLine()) != null) {
--- src/ChatServer.java
+++ src/ChatServer.java
@@ -9,9 +9,7 @@ import java.net.Socket;
 import java.net.ServerSocket;
 import java.util.concurrent.ThreadLocalRandom;
 
-/*
- * A chat server that delivers public and private messages.
- */
+
 public class ChatServer {
 
     // The server socket.
@@ -34,20 +32,13 @@ public class ChatServer {
             portNumber = Integer.valueOf(args[0]).intValue();
         }
 
-        /*
-         * Open a server socket on the portNumber (default 2222). Note that we can
-         * not choose a port less than 1023 if we are not privileged users (root).
-         */
         try {
             serverSocket = new ServerSocket(portNumber);
         } catch (IOException e) {
             System.out.println(e);
         }
 
-        /*
-         * Create a client socket for each connection and pass it to a new client
-         * thread.
-         */
+
         while (true) {
             try {
                 clientSocket = serverSocket.accept();
@@ -71,15 +62,7 @@ public class ChatServer {
     }
 }
 
-/*
- * The chat client thread. This client thread opens the input and the output
- * streams for a particular client, ask the client's name, informs all the
- * clients connected to the server about the fact that a new client has joined
- * the chat room, and as long as it receive data, echos that data back to all
- * other clients. The thread broadcast the incoming messages to all clients and
- * routes the private message to the particular client. When a client leaves the
- * chat room this thread informs also all the clients about that and terminates.
- */
+
 class clientThread extends Thread {
 
     private String clientName = null;
@@ -109,9 +92,7 @@ class clientThread extends Thread {
         }
 
         try {
-            /*
-             * Create input and output streams for this client.
-             */
+
             is = new DataInputStream(clientSocket.getInputStream());
             os = new PrintStream(clientSocket.getOutputStream());
 
@@ -135,7 +116,7 @@ class clientThread extends Thread {
                 }
             }
 
-            /* Welcome the new the client. */
+
             os.println("Welcome " + name
                     + " to the conversation.\nTo leave enter /quit in a new line.");
             synchronized (this) {
@@ -151,7 +132,8 @@ class clientThread extends Thread {
                     }
                 }
             }
-            /* Start the conversation. */
+
+
             while (true) {
                 String line = is.readLine();
                 try {
@@ -163,7 +145,6 @@ class clientThread extends Thread {
                 }
 
 
-                /* If the message is private sent it to the given client. */
                 if (line.startsWith("@")) {
                     String[] words = line.split("\\s", 2);
                     if (words.length > 1 && words[1] != null) {
@@ -175,10 +156,7 @@ class clientThread extends Thread {
                                             && threads[i].clientName != null
                                             && threads[i].clientName.equals(words[0])) {
                                         threads[i].os.println("[" + name + "] " + words[1]);
-                                        /*
-                                         * Echo this message to let the client know the private
-                                         * message was sent.
-                                         */
+
                                         this.os.println(">" + name + "> (to:" + words[0] + ")" + words[1]);
                                         break;
                                     }
@@ -187,7 +165,7 @@ class clientThread extends Thread {
                         }
                     }
                 } else {
-                    /* The message is public, broadcast it to all other clients. */
+
                     synchronized (this) {
                         for (int i = 0; i < maxClientsCount; i++) {
                             if (threads[i] != null && threads[i].clientName != null) {
@@ -208,10 +186,6 @@ class clientThread extends Thread {
             }
             os.println("*** Goodbye " + name + " ***");
 
-            /*
-             * Clean up. Set the current thread variable to null so that a new client
-             * could be accepted by the server.
-             */
             synchronized (this) {
                 for (int i = 0; i < maxClientsCount; i++) {
                     if (threads[i] == this) {
@@ -219,9 +193,7 @@ class clientThread extends Thread {
                     }
                 }
             }
-            /*
-             * Close the output stream, close the input stream, close the socket.
-             */
+
             is.close();
             os.close();
             clientSocket.close();
